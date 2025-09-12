d0e5bdcd3f39d8694bde93f2f90ec6cc9652fc22
Sandra Nyambura <sandranyambura62@gmail.com>
2025-09-12 17:53:24 +0300

    dashboards complete

--- Frontend/EDUFAM/src/App.tsx
+++ Frontend/EDUFAM/src/App.tsx
@@ -1,21 +1,24 @@
 import "./App.css";
 // import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/clerk-react";
-import CustomNavbar from "./components/Navbar"; // Import the Navbar component
-import ParentDashboard from "./components/ParentDashboard"; // Import the ParentDashboard component
-import Footer from "./components/Footer"; // Import the Footer component
+import CustomNavbar from "./components/Navbar";
+import ParentDashboard from "./components/ParentDashboard";
+import TeacherDashboard from "./components/TeacherDashboard";
+import Footer from "./components/Footer";
+import { useUser } from "@clerk/clerk-react";
 
 function App() {
-  // const { user } = useUser();
-  // const teacherEmail = "sandranyambura72@gmail.com";
+  const { user } = useUser();
+  const teacherEmail = "sandranyambura72@gmail.com";
+  const isTeacher = user?.primaryEmailAddress?.emailAddress === teacherEmail;
   return (
     <>
-      <CustomNavbar /> {/* Render the Navbar component */}
+      <CustomNavbar />
       <div className="flex-grow-1">
         <main>
-          <ParentDashboard /> {/* Render the ParentDashboard component */}
+          {isTeacher ? <TeacherDashboard /> : <ParentDashboard />}
         </main>
       </div>
-      <Footer /> {/* Render the Footer component */}
+      <Footer />
     </>
   );
 }
