1e340a1a220a676c2014a8863b609603076ace05
omwanzak <faithomwanza14@gmail.com>
2025-09-18 13:24:23 +0300

    Add admin dashboard components with sidebar navigation and configuration

---
+++ Frontend/EDUFAM/src/pages/dashboard/index.ts
@@ -0,0 +1,8 @@
+// Dashboard components exports
+export { default as AdminView } from './AdminView';
+export { default as AdminDashboard } from './AdminDashboard';
+
+// You can add other dashboard views here as they are created
+// export { default as TeacherView } from './TeacherView';
+// export { default as ParentView } from './ParentView';
+// export { default as StudentView } from './StudentView';
